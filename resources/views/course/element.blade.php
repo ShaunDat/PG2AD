278ba7837714e919f90d90fb7a225c65a24a17ca
ViDat2012 <Mrdatchannel@gmail.com>
2022-12-17 17:58:33 +0700

    nhat anh da commit

--- resources/views/course/element.blade.php
+++ resources/views/course/element.blade.php
@@ -12,15 +12,3 @@
     <div class="col-lg-10">
         <textarea name="note" class="form-control" rows="5">{{ isset($course->note) ? $course->note:'' }}</textarea></div>
 </div>
-
-<div class="form-group">
-    <label for="inputPhoto" class="col-form-label">Photo <span class="text-danger">*</span></label>
-    <div class="input-group">
-        <span class="input-group-btn">
-            <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
-            <i class="fa fa-picture-o"></i> Choose
-            </a>
-        </span>
-    <input id="thumbnail" class="form-control" type="file" name="photo" value="{{old('photo')}}">
-  </div>
-</div>
